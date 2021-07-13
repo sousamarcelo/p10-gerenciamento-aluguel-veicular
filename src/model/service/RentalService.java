2f7063cc3960883c536271b073cdbc0509b8f081
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-13 19:05:57 -0300

    Solution with interface

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -8,10 +8,10 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 	
 	
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
